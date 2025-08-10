9e36dea6b6eac64226bb80662d99d29b68865a33
RenZer0Z <99102974+RenZer0Z@users.noreply.github.com>
2025-08-09 23:51:43 -0700

    app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Bienvenido a Erickify PRO</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0a0a0a',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#1affd5',
+    fontSize: 28,
+    fontWeight: '900',
+  },
+});
